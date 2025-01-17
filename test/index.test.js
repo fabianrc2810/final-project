15be14f8e51892afbb550a4dc54004b784cb4f29
freyes <fabianhr.fr@gmail.com>
2025-01-17 13:51:19 -0500

    Initial commit

---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+import { expect, test, describe } from 'vitest';
+import request from 'supertest';
+import app from '../index.js';
+
+describe('GET /hello', () => {
+  test('responds with Hello World!', async () => {
+    const res = await request(app).get('/hello');
+    expect(res.status).toBe(200);
+    expect(res.text).toBe('Hello World');
+  });
+});
