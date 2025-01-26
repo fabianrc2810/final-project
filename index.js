9f72651f65a2be6ec0746cb4acaf5c599581c3a1
freyes <fabianhr.fr@gmail.com>
2025-01-26 10:36:42 -0500

    feat: new message from API

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/hello", (_req, res) => {
-  res.send("Hello World");
+  res.send("Hello World from Express!");
 });
 
 app.listen(port, () => {
