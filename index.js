530cc5efaf398ec65cbda47d3c1f62daf7650220
freyes <fabianhr.fr@gmail.com>
2025-01-28 13:05:52 -0500

    fix: provocar error linter

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 const app = express();
 
 const port = 3000;
+const unusedVariable = 'This will trigger a lint error';
 
 app.get("/hello", (_req, res) => {
   res.send("Hello World from Barcelona!");
